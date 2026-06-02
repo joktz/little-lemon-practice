4c42bf2cbab8bfb0325e082d85ec942edd0e41c3
joktz <jonathankrtz@gmail.com>
2026-06-02 12:23:53 +0900

    Initial setup for module 5 assignment.

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const WelcomeScreen = ({ navigation }) => {
+  // Add welcome screen code here.
+  return <View>
+    
+  </View>;
+};
+
+export default WelcomeScreen;
